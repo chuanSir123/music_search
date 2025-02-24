4a9ee58da696fcbae2c690f7ba470e99e3f0f167
黄传 <huangchuan@xuebangsoft.com>
2025-02-24 13:12:55 +0800

    加select

--- music_search/music_searcher.py
+++ music_search/music_searcher.py
@@ -3,7 +3,7 @@ import json
 import re
 from typing import Dict, Any, List
 from bs4 import BeautifulSoup
-from framework.logger import get_logger
+from kirara_ai.logger import get_logger
 
 logger = get_logger("MusicSearcher")
 
